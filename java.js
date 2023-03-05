d8102d129bd7997f2f584ad88f62ed027fecc6c3
MINNAKHALEED <minnakhaled12@gmail.com>
2023-03-05 18:57:30 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,15 @@
+const header= document.querySelector("header")
+
+window.addEventListener("scroll", (eo) => {
+    header.classList.toggle("sticky", this.window.scrollY > 0)
+})
+
+let menu= document.querySelector("#menu-icon")
+let navmenu= document.querySelector(".navmenu")
+
+menu.addEventListener("click", (eo) => {
+    menu.classList.toggle('bx-x')
+    navmenu.classList.toggle('open')
+
+})
+
